c1895129df6d5ec78fa17624f49c3cfd5d51e96d
rubygames21 <simon.portier@insa-rennes.fr>
2022-11-07 10:46:08 +0100

    first commit

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+
+public class Client {
+
+    public WebClient creatWebClient(BrowserVersion browserVersion, Boolean js, Boolean css, Boolean ssl){
+        WebClient client = new WebClient(browserVersion);
+        client.getOptions().setJavaScriptEnabled(js);
+        client.getOptions().setCssEnabled(css);
+        client.getOptions().setUseInsecureSSL(ssl);
+        return client;
+    }
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static String URL = "https://google.com";
+
+    public static void main(String[] args) throws IOException {
+        Client client = new Client();
+        WebClient webClient = client.creatWebClient(BrowserVersion.CHROME,false,true,true);
+        HtmlPage page = webClient.getPage(URL);
+        System.out.println(page.asXml());
+    }
+}
