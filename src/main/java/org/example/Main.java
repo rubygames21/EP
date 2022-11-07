f77b9385f49c7e06a14d311fbfda242c0539d74e
rubygames21 <simon.portier@insa-rennes.fr>
2022-11-07 11:05:48 +0100

    find button se connecter

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,28 @@ package org.example;
 
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.Html;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
 
-    public static String URL = "https://google.com";
+    public static String URL = "https://doodle.com/fr/";
 
     public static void main(String[] args) throws IOException {
         Client client = new Client();
-        WebClient webClient = client.creatWebClient(BrowserVersion.CHROME,false,true,true);
+        WebClient webClient = client.creatWebClient(BrowserVersion.CHROME,false,false,false);
         HtmlPage page = webClient.getPage(URL);
-        
+        System.out.println(page.asXml());
+        HtmlElement element = (HtmlElement) page.getElementById("header-log-in");
+        List<HtmlElement> elements = page.getTabbableElements();
+        System.out.println("-------------------------------------------------------");
+        for(HtmlElement elementI : elements){
+            System.out.println(elementI.asXml());
+        }
     }
 }
