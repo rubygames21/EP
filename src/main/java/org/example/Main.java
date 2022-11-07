f377aa7e79b7219780823b132dcb931a29ebccc4
rubygames21 <simon.portier@insa-rennes.fr>
2022-11-07 13:01:55 +0100

    essaye click boutton se connecter doodle

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -25,5 +25,23 @@ public class Main {
         for(HtmlElement elementI : elements){
             System.out.println(elementI.asXml());
         }
+        System.out.println("-------------------------------------------------------");
+        HtmlButton buttonLogin = (HtmlButton) elements.get(2);
+        System.out.println(buttonLogin.asXml());
+        System.out.println("-------------------------------------------------------");
+        HtmlPage page2 = buttonLogin.click();
+        System.out.println(buttonLogin.click().isHtmlPage());
+        System.out.println(page.asXml()==page2.asXml());
+        System.out.println(page2.getUrl());
+        System.out.println("-------------------------------------------------------");
+        System.out.println(buttonLogin.getNameAttribute());
+        System.out.println("-------------------------------------------------------");
+        /*System.out.println(page2.asXml());
+        List<HtmlElement> elements2 = page2.getTabbableElements();
+        System.out.println("-------------------------------------------------------");
+        for(HtmlElement elementI : elements2){
+            System.out.println(elementI.asXml());
+            System.out.println("***************************");
+        }*/
     }
 }
