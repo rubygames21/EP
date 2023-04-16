2e1c2c307cb2d6e1f1db6ebdc7f83a67b96d9525
clemadl <adolclement19@gmail.com>
2023-04-16 12:26:41 +0200

    ajout bouton sync et temps avant prochaine synchronisation qui évolue

--- src/main/java/com/example/noodleapp/HomeViewController.java
+++ src/main/java/com/example/noodleapp/HomeViewController.java
@@ -1,5 +1,8 @@
 package com.example.noodleapp;
 
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.value.ChangeListener;
 
 
@@ -7,16 +10,24 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class HomeViewController implements ChangeListener<Integer>{
 
-    private ObservableList<Integer> framPollsList;
+    private ObservableList framPollsList;
     private ObservableList<Integer> eventoPollsList;
     private ObservableList<Integer> doodlePollsList;
     @FXML
@@ -27,11 +38,21 @@ public class HomeViewController implements ChangeListener<Integer>{
     private ListView<Integer> listViewDoodle;
 
     @FXML
+    private Label timeSync;
+
+    @FXML
     private VBox centerVBox;
     @FXML
     public void initialize(){       //se lance dès le chargement de la scène automatique
 
-        framPollsList = FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11);
+        //recupération de la liste de sondages dans FramadateSrapper
+        FramadateScrapper fscrapper=new FramadateScrapper();
+        ArrayList liste = (ArrayList) fscrapper.getFpolls();
+
+        //ajout de quelques sondages
+        framPollsList = FXCollections.observableArrayList(liste);
+        //framPollsList = FXCollections.observableArrayList(1,2,3,4,5,6,7,8,9,10,11);
+
         eventoPollsList = FXCollections.observableArrayList(1,2,3,4,5,6,7,8);
         doodlePollsList = FXCollections.observableArrayList(1,2,3,4,5,6,7,8);
 
@@ -43,7 +64,7 @@ public class HomeViewController implements ChangeListener<Integer>{
             @Override
             public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer t1) {
                 //System.out.println("Vous avez sélectionné : " + newValue);
-                System.out.println("Mise à jour");
+                System.out.println("Vous avez sélectionné : " + t1);
             }
         });
 
@@ -51,15 +72,15 @@ public class HomeViewController implements ChangeListener<Integer>{
             @Override
             public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer t1) {
                 //System.out.println("Vous avez sélectionné : " + newValue);
-                System.out.println("Mise à jour");
+                System.out.println("Vous avez sélectionné : " + t1);
             }
         });
 
         listViewDoodle.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
             @Override
             public void changed(ObservableValue<? extends Integer> observableValue, Integer integer, Integer t1) {
-                // System.out.println("Vous avez sélectionné : " + newValue);
-                System.out.println("Mise à jour");
+                System.out.println("Vous avez sélectionné : " + t1);
+
             }
         });
         /*fPollsList.add(1);
@@ -71,9 +92,34 @@ public class HomeViewController implements ChangeListener<Integer>{
 
         System.out.println("Initialisation ok");
 
-        listViewFramadate.setItems(framPollsList); //cast obligatoire mais pas très légal
+        listViewFramadate.setItems(framPollsList);
         listViewEvento.setItems(eventoPollsList);
         listViewDoodle.setItems(doodlePollsList);
+    //gestion du temps de synchronisation
+
+        final int[] timer = {3703};
+        Timeline loop4 = new Timeline(new KeyFrame(Duration.seconds(1), new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent arg) {
+                timer[0]--;
+                int hour = (int)timer[0]/3600;  //on prend la partie entière
+                int minutes = (int)(timer[0]-hour*3600)/60;
+                int seconds = (timer[0]-hour*3600-minutes*60);
+                String time = "";
+                if(hour!=0){
+                    time+=hour+" h ";
+                }
+                if(minutes!=0){
+                    time+=minutes+" m ";
+                }
+                if(seconds!=0){
+                    time+=seconds+" s ";
+                }
+                timeSync.setText("Temps restant avant synchronisation :  " + time);
+            }
+        }));
+        loop4.setCycleCount(Timeline.INDEFINITE);
+        loop4.play();
+
 
 
     }
